f206a16a58f1edd550482cb52db750eeee23b044
MinhNguyenCG <info.nguyen1104@gmail.com>
2025-08-19 08:27:11 +0700

    demo lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from "react";
+import { StrictMode, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import HomePage from "@pages/HomePage.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MovieDetailPage from "@pages/MovieDetailPage.jsx";
+// import MovieDetailPage from "@pages/MovieDetailPage.jsx";
 import RootLayout from "@pages/RootLayout.jsx";
 import TVShowDetailPage from "@pages/TVShowDetailPage.jsx";
 import ModalProvider from "@components/context/ModalProvider.jsx";
 import PeopleDetailPage from "@pages/PeopleDetailPage.jsx";
+const MovieDetailPage = lazy(() => import("@pages/MovieDetailPage.jsx"));
+
 const router = createBrowserRouter([
   {
     element: <RootLayout />,
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "@components/Header";
+import LoadingPage from "@components/Loading";
+import { Suspense } from "react";
 
 const RootLayout = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <Suspense fallback={<LoadingPage />}>
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
